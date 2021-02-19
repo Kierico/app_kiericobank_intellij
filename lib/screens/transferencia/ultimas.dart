625ed8dad019b578f7bc0bfbf91dbd910d689252
Kiérico Souza <65201369+Kierico@users.noreply.github.com>
2021-02-19 19:41:58 -0400

    Aula 05 - Validando e finalizando o app: Validações Finais.

--- lib/screens/transferencia/ultimas.dart
+++ lib/screens/transferencia/ultimas.dart
@@ -18,20 +18,28 @@ class UltimasTransferencias extends StatelessWidget {
             fontSize: 24,
           ),
         ),
-        Consumer<Transferencias>(
-          builder: (context, transferencias, child) {
-            return ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: 2,
-              shrinkWrap: true,
-              itemBuilder: (context, indice) {
-                return ItemTransferencia(transferencias.transferencias[indice]);
-              },
-            );
+        Consumer<Transferencias>(builder: (context, transferencias, child) {
+          final _ultimasTransferencias =
+              transferencias.transferencias.reversed.toList();
+          final _quantidade = transferencias.transferencias.length;
+          int tamanho = 3;
+          if (_quantidade == 0) {
+            return SemTransferenciaCadastrada();
           }
-        ),
+          if (_quantidade < 3) {
+            tamanho = _quantidade;
+          }
+          return ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: tamanho,
+            shrinkWrap: true,
+            itemBuilder: (context, indice) {
+              return ItemTransferencia(_ultimasTransferencias[indice]);
+            },
+          );
+        }),
         RaisedButton(
-          child: Text('Transferências'),
+          child: Text('Ver todas Transferências'),
           onPressed: () {
             Navigator.push(
               context,
@@ -42,6 +50,22 @@ class UltimasTransferencias extends StatelessWidget {
           },
         ),
       ],
+    );
+  }
+}
+
+class SemTransferenciaCadastrada extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(40),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Você ainda não cadastrou nenhuma transferência',
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
